b108c5f3a539759442c0e16fbfe393b8bf4ea9c7
RagedHavoc <annie.johnson1we@gmail.com>
2020-03-04 23:16:23 +0000

    Your message here

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-
+var custom_api_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY";
 $("#search-button").click(function(){
+  fetch(custom_api_url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+          
   
-  
-  
+      });
 });
 
